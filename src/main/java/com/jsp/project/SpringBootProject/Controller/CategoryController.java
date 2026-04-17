e93e4ecdc6b11991122e5bd3d5b0eb77be388e1d
Nisith69 <bhowmiknisith96@gmail.com>
2026-04-18 00:25:54 +0530

    first commit

--- src/main/java/com/jsp/project/SpringBootProject/Controller/CategoryController.java
+++ src/main/java/com/jsp/project/SpringBootProject/Controller/CategoryController.java
@@ -5,14 +5,12 @@ import com.jsp.project.SpringBootProject.Service.CategoryService;
 import com.jsp.project.SpringBootProject.dto.ResponseStructure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
